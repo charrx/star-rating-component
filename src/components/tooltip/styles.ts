a0212255a4fe8b3f22f444c5c56c879efc7a01ad
charrx <leanygaard97@gmail.com>
2024-02-28 01:26:31 +0100

    refactor: small adjustsments and clean up

--- src/components/tooltip/styles.ts
+++ src/components/tooltip/styles.ts
@@ -6,22 +6,27 @@ export const tooltipStyles: CSSResultGroup = [
       visibility: var(--tooltip-visibility, hidden);
       --tooltip-background-color: #3b3b3a;
       --tooltip-color: #fff;
+      --tooltip-padding: 0.4375rem 0.625rem 0.4375rem 0.6875rem;
+      --tooltip-border-radius: 0.25rem;
+      --tooltip-font-size: 0.75rem;
+      --tooltip-box-shadow: 0rem 0.0625rem 0.125rem 0rem rgba(0, 0, 0, 0.25);
     }
 
     .tooltip {
       background-color: var(--tooltip-background-color);
       color: var(--tooltip-color);
-      padding: 0.75rem 0.8rem 0.75rem 1rem;
-      border-radius: 4px;
-      letter-spacing: 3px;
-      font-size: 0.75rem;
-      font-weight: 500;
+      padding: var(--tooltip-padding);
+      border-radius: var(--tooltip-border-radius);
+      letter-spacing: 1px;
+      font-size: var(--tooltip-font-size);
+      font-weight: 400;
       z-index: 1;
       left: 50%;
       position: absolute;
-      box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.25);
+      box-shadow: var(--tooltip-box-shadow);
       bottom: 125%;
       transform: translateX(-50%);
+      text-align: center;
     }
 
     .tooltip::after {
@@ -29,8 +34,8 @@ export const tooltipStyles: CSSResultGroup = [
       position: absolute;
       top: 100%;
       left: 50%;
-      margin-left: -5px;
-      border-width: 5px;
+      margin-left: -0.3125rem;
+      border-width: 0.3125rem;
       border-style: solid;
       border-color: var(--tooltip-background-color) transparent transparent
         transparent;
